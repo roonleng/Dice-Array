27dfddfce101acdbc378dbd1e5f482cecab67d76
roonleng <42977073+roonleng@users.noreply.github.com>
2018-10-26 13:37:12 -0400

    Add files via upload

---
+++ DiceArray/src/DiceArray.java
@@ -0,0 +1,16 @@
+
+public class DiceArray 
+{
+	//fields
+	int roll;
+	int total;
+	DiceRoll new = dice1.DiceRoll();
+	DiceRoll new = dice2.DiceRoll();
+	//
+	for (i=0; i<=1000; i++)
+	{
+		total = dice1.roll() + dice2.roll();
+		if (total==2)
+		
+	}
+}
