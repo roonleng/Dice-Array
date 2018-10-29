0f1fb1c0b3cc4539cf6122d8b481d42bec5fdfe3
roonleng <42977073+roonleng@users.noreply.github.com>
2018-10-28 23:16:15 -0400

    Add files via upload

--- DiceArray/src/DiceArray.java
+++ DiceArray/src/DiceArray.java
@@ -1,16 +1,27 @@
-
 public class DiceArray 
 {
-	//fields
-	int roll;
-	int total;
-	DiceRoll new = dice1.DiceRoll();
-	DiceRoll new = dice2.DiceRoll();
-	//
-	for (i=0; i<=1000; i++)
+	public static void main (String[] args)
 	{
+	//fields
+	int roll1;
+	int roll2;
+	int total;
+	DiceRoll dice1 = new DiceRoll();
+	DiceRoll dice2 = new DiceRoll();
+	int[] value = new int[13];
+	//
+	for (int i=0; i<=1000; i++)
+	{
+		roll1 = dice1.roll();
+		roll2 = dice2.roll();
 		total = dice1.roll() + dice2.roll();
-		if (total==2)
+		value [total]+=1;
+	for (int j=2; j<13; j++)
+	{
+		System.out.println("You rolled " + j + value[j] + " times");
+	}
 		
 	}
+	}
 }
+
